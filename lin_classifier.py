07d965e69c0904d9dd738d63d9d98d3add2b1998
bjornrho <bjornrho@ntnu.no>
2020-03-28 13:04:02 +0100

    read data from files

--- lin_classifier.py
+++ lin_classifier.py
@@ -5,3 +5,24 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from matplotlib import style
 
+attribute_names = ["sepal_length", "sepal_width", "petal_length", "petal_width"]
+class_data = [
+    pd.read_csv("class_1", names=attribute_names), 
+    pd.read_csv("class_2", names=attribute_names),
+    pd.read_csv("class_3", names=attribute_names)]
+
+
+print(class_data)
+
+
+"""
+def predict()
+
+def MSE()
+
+def train()
+
+def run()
+"""
+
+print("shitboy")
